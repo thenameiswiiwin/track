34a03f93525d1fe35f2dbdc719f3f8a843d52df1
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-05 04:50:29 -0500

    seeding user and playlist data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
@@ -16,6 +17,37 @@ const run = async () => {
               name: song.name,
               duration: song.duration,
               url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync(process.env.EXAMPLE_PASSWORD, salt),
+      firstName: "Huy",
+      lastName: "Nguyen",
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
             })),
           },
         },
