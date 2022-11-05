0d3298f4f00cdb91a71d2f72a49f6cfc4bfc2156
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-04 20:21:00 -0500

    installed packages, setup tailwind, eslint config, typescript config, and babelrc.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -67,5 +67,7 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
