b1912fed5417b96d07097f93697afbfadc9175da
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-05 01:10:42 -0500

    change margin

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,8 +1,12 @@
+import Sidebar from './Sidebar';
+
 const PlayerLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen">
-      <div className="absolute top-0 left-0 w-64">sidebar</div>
-      <div className="ml-64 mb-40">{children}</div>
+      <div className="absolute top-0 left-0 w-64">
+        <Sidebar />
+      </div>
+      <div className="ml-64 mb-24">{children}</div>
       <div className="absolute left-0 bottom-0">player</div>
     </div>
   );
