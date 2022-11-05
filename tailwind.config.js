5bd125931677add13e752b321a98ec1d935c3b33
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-05 02:52:32 -0500

    styled scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
@@ -8,5 +11,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        '.scrollbar': {
+          overflowY: 'auto',
+          scrollbarColor: `${theme('colors.gray.500')} ${theme(
+            'colors.gray.400'
+          )}`,
+          scrollbarWidth: 'thin',
+        },
+        '.scrollbar::-webkit-scrollbar': {
+          width: '8px',
+        },
+        '.scrollbar::-webkit-scrollbar-thumb': {
+          backgroundColor: theme('colors.gray.500'),
+          borderRadius: '25px',
+        },
+        '.scrollbar::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: theme('colors.gray.400'),
+        },
+        '.scrollbar::-webkit-scrollbar-track-piece': {
+          backgroundColor: theme('colors.black'),
+        },
+      });
+    }),
+  ],
 };
