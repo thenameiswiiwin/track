b068d6a373b5cc8f203fde0c819b95789ee64fd4
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-10 04:52:50 -0600

    created Logo and Spinner components

---
+++ components/Logo.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import Image from "next/image";
+
+const Logo: FC<{ text: string; bg: string }> = ({ text, bg }) => {
+  return (
+    <>
+      <Image
+        src="/logo.svg"
+        height={40}
+        width={80}
+        alt="track logo"
+        className={`bg-${bg}`}
+      />
+      <h1 className={`ml-3 font-mono text-4xl font-bold italic text-${text}`}>
+        Track
+      </h1>
+    </>
+  );
+};
+
+export default Logo;
---
+++ components/Spinner.tsx
@@ -0,0 +1,7 @@
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+
+const Spinner = () => {
+  return <GlobeAltIcon className="ml-2 h-6 w-6 animate-spin" />;
+};
+
+export default Spinner;
