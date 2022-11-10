9dfc7c8a5e7dfbb29248fdfaebe6d0cbe637ece2
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-10 05:01:45 -0600

    eslint stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -14,25 +14,25 @@ module.exports = {
   plugins: [
     plugin(({ addBase, theme }) => {
       addBase({
-        '.scrollbar': {
-          overflowY: 'auto',
-          scrollbarColor: `${theme('colors.gray.500')} ${theme(
-            'colors.gray.400'
+        ".scrollbar": {
+          overflowY: "auto",
+          scrollbarColor: `${theme("colors.gray.500")} ${theme(
+            "colors.gray.400"
           )}`,
-          scrollbarWidth: 'thin',
+          scrollbarWidth: "thin",
         },
-        '.scrollbar::-webkit-scrollbar': {
-          width: '8px',
+        ".scrollbar::-webkit-scrollbar": {
+          width: "8px",
         },
-        '.scrollbar::-webkit-scrollbar-thumb': {
-          backgroundColor: theme('colors.gray.500'),
-          borderRadius: '25px',
+        ".scrollbar::-webkit-scrollbar-thumb": {
+          backgroundColor: theme("colors.gray.500"),
+          borderRadius: "25px",
         },
-        '.scrollbar::-webkit-scrollbar-thumb:hover': {
-          backgroundColor: theme('colors.gray.400'),
+        ".scrollbar::-webkit-scrollbar-thumb:hover": {
+          backgroundColor: theme("colors.gray.400"),
         },
-        '.scrollbar::-webkit-scrollbar-track-piece': {
-          backgroundColor: theme('colors.black'),
+        ".scrollbar::-webkit-scrollbar-track-piece": {
+          backgroundColor: theme("colors.black"),
         },
       });
     }),
